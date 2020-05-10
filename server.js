6c87a26b4e907c4b5a44d77bfc99233e8165fcb2
Matthew Berhe <matthew.berhe1@gmail.com>
2020-05-10 19:50:12 -0400

    updating server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose
 
 if (process.env.NODE_ENV === 'production') {
 	app.use((req, res, next) => {
-		if (req.header('x-forwarded-proto') !== 'https') {
+		if (req.header('x-forwarded-proto') !== 'http') {
 			res.redirect(`https://${req.hostname}${req.url}`)
 		} else {
 			next()
